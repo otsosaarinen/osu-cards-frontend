c5025b0ca933622306b2e4117a65f02058d6ce59
Otso Saarinen <saarinenotso@gmail.com>
2025-02-21 12:00:37 +0200

    Update App.tsx

--- osu-cards-frontend/src/App.tsx
+++ osu-cards-frontend/src/App.tsx
@@ -1,11 +1,18 @@
-function App() {
+import OsuPlayerCard from "./OsuPlayerCard";
+
+const App = () => {
     return (
-        <>
-            <div className="flex justify-center items-center h-screen">
-                osu! cards
-            </div>
-        </>
+        <div className="flex justify-center items-center min-h-screen bg-gray-950">
+            <OsuPlayerCard
+                username="Vaxei"
+                avatar="https://a.ppy.sh/4787150.png"
+                rank={2}
+                pp={14000}
+                accuracy={99.56}
+                country="US" // ISO country code
+            />
+        </div>
     );
-}
+};
 
 export default App;
