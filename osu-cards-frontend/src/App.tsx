787c0b7a4912b2f948682f197b20609a4b9a90fc
Otso Saarinen <saarinenotso@gmail.com>
2025-02-21 12:51:49 +0200

    Update App.tsx

--- osu-cards-frontend/src/App.tsx
+++ osu-cards-frontend/src/App.tsx
@@ -2,7 +2,7 @@ import OsuPlayerCard from "./OsuPlayerCard";
 
 const App = () => {
     return (
-        <div className="flex justify-center items-center min-h-screen bg-gray-950">
+        <div className="flex justify-center items-center min-h-screen bg-gray-950 gap-5">
             <OsuPlayerCard
                 username="Aetherian"
                 avatar="https://a.ppy.sh/6263040"
@@ -10,6 +10,14 @@ const App = () => {
                 pp={6679}
                 accuracy={98.56}
                 country="FI"
+            />
+            <OsuPlayerCard
+                username="Cookiezi"
+                avatar="https://a.ppy.sh/1000140.png"
+                rank={1}
+                pp={25000}
+                accuracy={98.99}
+                country="KR"
             />
         </div>
     );
