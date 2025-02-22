83071a5a0a41eb36465c46f589a807b078828d2a
Otso Saarinen <saarinenotso@gmail.com>
2025-02-22 12:18:14 +0200

    Update OsuPlayerCard.tsx

--- osu-cards-frontend/src/OsuPlayerCard.tsx
+++ osu-cards-frontend/src/OsuPlayerCard.tsx
@@ -22,18 +22,18 @@ const OsuPlayerCard: React.FC<OsuPlayerCardProps> = ({
 }) => {
     const rarityColor =
         rarity === "Legendary"
-            ? "bg-red-400 shadow-md shadow-red-400/50"
+            ? "bg-linear-to-b from-red-400 to-pink-400 shadow-md shadow-red-400/50"
             : rarity === "Ultra Rare"
-            ? "bg-fuchsia-400 shadow-md shadow-fuchsia-400/50"
+            ? "bg-linear-to-b from-fuchsia-400 to-violet-400 shadow-md shadow-fuchsia-400/50"
             : rarity === "Rare"
-            ? "bg-sky-400 shadow-md shadow-sky-400/50"
+            ? "bg-linear-to-b from-sky-400 to-cyan-400 shadow-md shadow-sky-400/50"
             : "bg-slate-400 shadow-md shadow-slate-400/50";
 
     return (
         <>
             {/* Player card container */}
             <div
-                className={`${rarityColor} text-black rounded-lg w-70 h-90 flex flex-col justify-start items-center transform transition-all duration-150 hover:scale-105 hover:shadow-lg`}
+                className={`${rarityColor} text-black border-3 border-white rounded-lg w-70 h-90 flex flex-col justify-start items-center transform transition-all duration-150 hover:scale-105 hover:shadow-lg`}
             >
                 <div className="font-bold uppercase">{rarity}</div>
                 {/* Player avatar */}
@@ -50,7 +50,7 @@ const OsuPlayerCard: React.FC<OsuPlayerCardProps> = ({
                 <div className="">{pp}pp</div>
                 <div className="">{accuracy}%</div>
                 <div className="">
-                    <img src={country} alt="country_flag" />
+                    <img src={country} alt="flag" />
                 </div>
             </div>
         </>
