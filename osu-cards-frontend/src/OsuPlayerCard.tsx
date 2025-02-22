490ac88969047c8a7f1391d0f00f2a2cbb52e49f
Otso Saarinen <saarinenotso@gmail.com>
2025-02-22 12:11:35 +0200

    Update OsuPlayerCard.tsx

--- osu-cards-frontend/src/OsuPlayerCard.tsx
+++ osu-cards-frontend/src/OsuPlayerCard.tsx
@@ -22,26 +22,26 @@ const OsuPlayerCard: React.FC<OsuPlayerCardProps> = ({
 }) => {
     const rarityColor =
         rarity === "Legendary"
-            ? "bg-red-400 shadow-lg shadow-red-400/50"
+            ? "bg-red-400 shadow-md shadow-red-400/50"
             : rarity === "Ultra Rare"
-            ? "bg-fuchsia-400 shadow-lg shadow-fuchsia-400/50"
+            ? "bg-fuchsia-400 shadow-md shadow-fuchsia-400/50"
             : rarity === "Rare"
-            ? "bg-sky-400 shadow-lg shadow-sky-400/50"
-            : "bg-slate-400 shadow-lg shadow-slate-400/50";
+            ? "bg-sky-400 shadow-md shadow-sky-400/50"
+            : "bg-slate-400 shadow-md shadow-slate-400/50";
 
     return (
         <>
             {/* Player card container */}
             <div
-                className={`${rarityColor} text-black rounded-lg w-70 h-90 flex flex-col justify-start items-center transform transition-all duration-150 hover:scale-105`}
+                className={`${rarityColor} text-black rounded-lg w-70 h-90 flex flex-col justify-start items-center transform transition-all duration-150 hover:scale-105 hover:shadow-lg`}
             >
-                <div className="">{rarity}</div>
+                <div className="font-bold uppercase">{rarity}</div>
                 {/* Player avatar */}
                 <div className="">
                     <img
                         src={avatar}
                         alt="profile_picture"
-                        className="w-full h-30 rounded-lg"
+                        className="w-full h-40 object-cover rounded-lg"
                     />
                 </div>
                 {/* Display player details */}
